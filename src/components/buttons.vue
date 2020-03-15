264be829c32842ec70d852b4561a95837d85abde
BISMINSHA1 <62139530+BISMINSHA1@users.noreply.github.com>
2020-03-15 20:05:00 +0530

    First commit

---
+++ src/components/buttons.vue
@@ -0,0 +1,65 @@
+<template>
+  <div>
+    <input
+      class="todoinput"
+      type="text"
+      placeholder="Enter new TODO"
+      v-model="newTodo"
+      @keypress.enter="addTodo(newTodo)"
+    />
+    <button @click="addTodo(newTodo)">ADD</button>
+    <div v-for="(todo, index) in todos" :key="index" class="todoitem">
+      <div>{{ todo }}</div>
+      <div class="removeitem" @click="removeTodo(index)">&times;</div>
+    </div>
+    <template v-if="todos.length == 0">
+      <h3>
+        No Todos
+      </h3>
+    </template>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      newTOdo: " ",
+      todos: []
+    };
+  },
+  methods: {
+    addTodo(todo) {
+      this.todos.push(todo);
+      this.newTodo = "";
+    },
+    removeTodo(index) {
+      this.todos.splice(index, 1);
+    }
+  }
+};
+</script>
+<style lang="scss">
+.todoinput {
+  width: 50%;
+  padding: 5px 5px;
+  font-size: 18px;
+  margin-bottom: 10px;
+  &:focus {
+    outline: 0;
+  }
+}
+.todoitem {
+  margin-bottom: 10px;
+  padding: 10px 10px;
+  display: flex;
+  justify-content: space-around;
+}
+.removeitem {
+  cursor: pointer;
+  margin-right: 14px;
+  &:hover {
+    color: black;
+  }
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <HelloWorld msg="TODO APP" />
+    <buttons />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+import buttons from "@/components/buttons.vue";
+
+export default {
+  components: {
+    HelloWorld,
+    buttons
+  }
+};
+</script>
+<style lang="sass"></style>
